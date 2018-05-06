88650d3fc9a2f8b43f6c0a6e170e21f1f0ed7b35
Yousha Rizvi <yousha.rizvi@tenpearls.com>
2018-05-06 21:06:53 +0500

    Repo init: Tasks Completed

---
+++ routes/vehicles.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const rp = require('request-promise');
+const router = express.Router();
+
+const API_URL = 'http://one.nhtsa.gov/webapi/api';
+
+function getURL(type) {
+  return {
+    safetyRatings: params =>
+      API_URL +
+      `/SafetyRatings/modelyear/${params.modelYear}/make/${params.manufacturer}/model/${params.model}?format=json`,
+    overallRating: VehicleId =>
+      API_URL + `/SafetyRatings/VehicleId/${VehicleId}?format=json`,
+
+  }[type](...Array.apply(null, arguments).slice(1));
+}
+
+function getSafetyRatings(payload) {
+  console.log(payload)
+  return rp
+    .get(getURL('safetyRatings', payload))
+    .then(a => {
+      console.log(JSON.parse(a));
+      return a;
+    })
+    .then(JSON.parse)
+    .then(response => ({
+      Count: response.Count,
+      Results: response.Results.map(vehicle => ({
+        Description: vehicle.VehicleDescription,
+        VehicleId: vehicle.VehicleId,
+      }))
+    }))
+    .catch(err => ({
+      Count: 0,
+      Results: []
+    }));
+}
+
+function getOverallRating(vehicleId) {
+  return rp
+    .get(getURL('overallRating', vehicleId))
+    .then(JSON.parse)
+    .then(res => res.Results[0].OverallRating)
+
+}
+
+/* GET users listing. */
+router.get('/:modelYear/:manufacturer/:model', function (req, res, next) {
+  getSafetyRatings(req.params)
+    .then(response => {
+      if (req.query.withRating && ['true', true, 1, '1'].indexOf(req.query.withRating) >= 0)
+        return Promise.all(response.Results.map(vehicle => getOverallRating(vehicle.VehicleId)
+          .then(CrashRating => Object.assign({}, vehicle, {
+            CrashRating
+          }))
+        ))
+      else return response;
+    })
+    .then(response => res.send(response))
+});
+
+router.post('/', function (req, res, next) {
+  getSafetyRatings(req.body)
+    .then(response => res.send(response))
+});
+
+
+module.exports = router;
